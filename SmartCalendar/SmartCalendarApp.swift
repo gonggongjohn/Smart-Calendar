001eab297e3b17933cc659e99377b041b5288665
gongongjohn <gonggongjohn@163.com>
2021-01-24 12:33:06 +0800

    init ios repo

---
+++ SmartCalendar/SmartCalendarApp.swift
@@ -0,0 +1,15 @@
+//
+//  SmartCalendarApp.swift
+//  SmartCalendar
+//
+
+import SwiftUI
+
+@main
+struct SmartCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
