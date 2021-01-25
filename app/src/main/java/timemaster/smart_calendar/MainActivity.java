193db6bfa59a9c9a803cb6c97800e0d63ecf179a
10194500031 <10194500031@stu.ecnu.edu.cn>
2021-01-25 22:09:45 +0800

    测试2

--- app/src/main/java/timemaster/smart_calendar/MainActivity.java
+++ app/src/main/java/timemaster/smart_calendar/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //123456
+        //123456
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnGeo = findViewById(R.id.btn_geo);
