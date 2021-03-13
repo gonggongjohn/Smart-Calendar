ea8f69c65915088620029030f21c9525aa285497
zhengbeini <10184810122@stu.ecnu.edu.cn>
2021-03-13 16:19:21 +0800

    Connect to the server

--- SmartCalendar/utils/UserUtils.swift
+++ SmartCalendar/utils/UserUtils.swift
@@ -7,6 +7,25 @@ import Foundation
 
 class UserUtils{
     public func login(server: String, username: String, password: String) -> Bool{
-        return true
+        var connected = false
+        var request = URLRequest(url: URL(string: server)!)
+        request.httpMethod = "GET"
+        let httpArg = "?username="+username+"&password="+password
+        request.httpBody = httpArg.data(using:.utf8)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) {(data, response, error) in
+            do {
+                if error != nil{
+                    print("Failed to connect to server.")
+                }else{
+                    let json = try!JSON(data:data!)
+                    if(json["status"]=="success"){
+                        connected = true
+                    }
+                }
+            }
+        }
+        task.resume()
+        return connected
     }
 }
