02f216e33841cec62c142fe3cc9929cf3546b68e
gongongjohn <gonggongjohn@163.com>
2021-03-14 17:55:30 +0800

    build the foundation of user management system

--- SmartCalendar/LoginView.swift
+++ SmartCalendar/LoginView.swift
@@ -6,22 +6,19 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State var server: String = ""
+    private var userUtils = UserUtils()
     @State var username: String = ""
     @State var password: String = ""
-    @State var isLoginSuccess: Bool = false
+    @State var isLoginSuccess = false
+    @State var showLoginFailInfo = false
+    @State var showRegSuccessInfo = false
+    @State var showRegFailInfo = false
     var body: some View {
         NavigationView{
             VStack(spacing: 50.0){
                 Text("Smart Calendar")
                     .font(.title)
                 VStack{
-                    HStack{
-                        Text("服务器地址：")
-                            .font(.title3)
-                        TextField("Host Address", text: $server)
-                            .font(.title3)
-                    }
                     HStack{
                         Text("用户名：")
                             .font(.title3)
@@ -35,18 +32,47 @@ struct LoginView: View {
                             .font(.title3)
                     }
                 }
-                NavigationLink(
-                    destination: MainView(),
-                    isActive: $isLoginSuccess) {
-                    Text("登陆")
-                        .onTapGesture {
-                            let user_utils = UserUtils()
-                            self.isLoginSuccess = user_utils.login(server: self.server, username: self.username, password: self.password)
+                HStack{
+                    NavigationLink(
+                        destination: MainView(),
+                        isActive: $isLoginSuccess) {
+                        Text("登陆")
+                            .onTapGesture {
+                                if self.userUtils.login(username: self.username, password: self.password) {
+                                    self.isLoginSuccess = true
+                                }
+                                else{
+                                    self.showLoginFailInfo = true
+                                }
+                            }
+                            .font(.title2)
+                            .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+                            .background(RoundedRectangle(cornerRadius: 10.0)
+                                            .stroke(Color.black, lineWidth: 2.0))
+                    }
+                    .alert(isPresented: $showLoginFailInfo){
+                        Alert(title: Text("用户登录系统"), message: Text("登录失败！"), dismissButton: .default(Text("确定")))
+                    }
+                    Button(action: {
+                        if self.userUtils.register(username: self.username, password: self.password){
+                            self.showRegSuccessInfo = true
                         }
-                        .font(.title2)
-                        .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
-                        .background(RoundedRectangle(cornerRadius: 10.0)
-                                        .stroke(Color.black, lineWidth: 2.0))
+                        else{
+                            self.showRegFailInfo = true
+                        }
+                    }){
+                        Text("注册")
+                            .font(.title2)
+                            .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+                            .background(RoundedRectangle(cornerRadius: 10.0)
+                                            .stroke(Color.black, lineWidth: 2.0))
+                    }
+                    .alert(isPresented: $showRegSuccessInfo){
+                        Alert(title: Text("用户登录系统"), message: Text("注册成功！"), dismissButton: .default(Text("确定")))
+                    }
+                    .alert(isPresented: $showRegFailInfo){
+                        Alert(title: Text("用户登录系统"), message: Text("注册失败！"), dismissButton: .default(Text("确定")))
+                    }
                 }
             }.padding()
         }
--- SmartCalendar/utils/UserUtils.swift
+++ SmartCalendar/utils/UserUtils.swift
@@ -6,12 +6,45 @@
 import Foundation
 
 class UserUtils{
-    public func login(server: String, username: String, password: String) -> Bool{
+    public func login(username: String, password: String) -> Bool{
         var connected = false
-        var request = URLRequest(url: URL(string: server)!)
+        var isGet = false
+        let server = "http://mc.mcgo.pw:40018/login"
+        let url_str = server + "?username=" + username + "&password=" + password
+        var request = URLRequest(url: URL(string: url_str)!)
         request.httpMethod = "GET"
-        let httpArg = "?username="+username+"&password="+password
-        request.httpBody = httpArg.data(using:.utf8)
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) {(data, response, error) in
+            do {
+                if error != nil{
+                    print("Failed to connect to server.")
+                }else{
+                    let json = try!JSON(data:data!)
+                    print(json["status"])
+                    if(json["status"] == "success"){
+                        connected = true
+                    }
+                }
+                isGet = true
+            }
+        }as URLSessionTask
+        task.resume()
+        while(!isGet){
+            
+        }
+        print(connected)
+        return connected
+    }
+    
+    public func register(username: String, password: String) -> Bool{
+        var connected = false
+        var isGet = false
+        let json: [String: Any] = ["username":username,"password":password]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        let server = "http://mc.mcgo.pw:40018/register"
+        var request = URLRequest(url:URL(string:server)!)
+        request.httpMethod = "POST"
+        request.httpBody = jsonData
         let session = URLSession.shared
         let task = session.dataTask(with: request) {(data, response, error) in
             do {
@@ -22,10 +55,15 @@ class UserUtils{
                     if(json["status"]=="success"){
                         connected = true
                     }
+                    isGet = true;
                 }
             }
         }
         task.resume()
+        while(!isGet){
+            
+        }
         return connected
     }
+
 }
